604b27a0e1c929702a70e56aecfa7abd52f0cbe6
tantai0809 <tttai.sgt@gmail.com>
2021-10-10 14:59:10 +0700

    modify footer section

--- app/web/wp-content/themes/sgt-wiki2/footer.php
+++ app/web/wp-content/themes/sgt-wiki2/footer.php
@@ -1,7 +1,12 @@
 <!--   footer section -->
-<div class="mx-auto">
-    <hr>
-    <p class="text-center py-3">© 2021 SaigonTech</p>
+<div class="bg-green-50 text-white py-3 px-4 text-center fixed left-0 bottom-0 right-0 z-40">
+    <div class="border-t-2 border-b-2 border-gray-300 flex flex-col items-center">
+        <div class="sm:w-2/3 text-center pt-2">
+            <p class="text-sm text-blue-700 font-bold mb-2">
+                © 2021 SaigonTech
+            </p>
+        </div>
+    </div>
 </div>
 <?php wp_footer();?>
 </body>
